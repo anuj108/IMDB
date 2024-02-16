f24357beb5d892483ae9dc273258e196022803be
AnUj <shakya.anuj910@gmail.com>
2024-02-16 12:28:13 +0530

    Override Function

--- Csharp/BeginnerAssignment/Question1/Program.cs
+++ Csharp/BeginnerAssignment/Question1/Program.cs
@@ -16,16 +16,19 @@ namespace Question1
             {
                 Console.WriteLine("ENTER A NUMBER OR TYPE \"OK\" TO EXIT");
                 string response = Console.ReadLine();
-                if (response=="ok")
-                {
-                    Console.WriteLine("THE SUM OF ENTERED NUMBERS IS : "+sum);
-                    return;
-                }
-                else
-                {
-                    int value = Convert.ToInt32(response);
-                    sum=sum+value;
-                }
+                
+                    if (response=="ok")
+                    {
+                        Console.WriteLine("THE SUM OF ENTERED NUMBERS IS : "+sum);
+                        return;
+                    }
+                    else
+                    {
+                        int value = Convert.ToInt32(response);
+                        sum=sum+value;
+                    }
+ 
+                
             }
            
         }
--- Csharp/BeginnerAssignment/Question2/Program.cs
+++ Csharp/BeginnerAssignment/Question2/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Question2
 {
+   
     internal class Program
     {
         static void Main(string[] args)
