7c7566dfc45b5a056b473083d1101fd8d0a509b5
AnUj <shakya.anuj910@gmail.com>
2024-02-16 10:57:00 +0530

    Intermediate-Assignment-C#

---
+++ Csharp/IntermediateAssignment/AdvanceCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IntermediateAssignment
+{
+    public interface IAdvanceCalculator
+    {
+        double Power(int b, int e);
+    }
+    public class AdvanceCalculator : Calculator
+    {
+        
+        public double Power(int b,int exp)
+        {
+            return ResultProp=Math.Pow(b, exp);
+        }
+
+        public double getResult()
+        {
+            return ResultProp*1000000;
+        }
+
+    }
+
+}
